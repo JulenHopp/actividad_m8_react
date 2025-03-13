a48678e4c032a13448ce7cb404d1315475a5e34c
eugeniogzc <eugenio.garza26@gmail.com>
2025-03-13 10:27:02 -0600

    creacion pag contacto

--- frontend/src/pages/contacto/Contacto.jsx
+++ frontend/src/pages/contacto/Contacto.jsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Contacto.css'; // Importamos el archivo de estilos
 
 const Contacto = () => {
+    const [mensajeMostrado, setMensajeMostrado] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setMensajeMostrado(true);
+    };
+
     return (
-        <p>Esta es la informacion de contacto</p>
+        <div className="contacto-container">
+            <h2>Contacto</h2>
+            <form onSubmit={handleSubmit} className="contacto-form">
+                <input type="text" placeholder="Nombre" required className="contacto-input" />
+                <input type="email" placeholder="Correo" required className="contacto-input" />
+                <button type="submit" className="contacto-btn">Enviar</button>
+            </form>
+
+            {mensajeMostrado && <p className="mensaje">Nos comunicaremos contigo en cuanto podamos!</p>}
+        </div>
     );
 };
 
